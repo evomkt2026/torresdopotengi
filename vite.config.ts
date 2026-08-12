d618d7013cdd3d1d0ca9f3d108c7c2be3c98c828
EVO Marketing <eliabe@agenciaevomkt.com.br>
2026-08-11 21:45:12 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,9 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  vite: {
-    base: "/torresdopotengi/",
-  },
+vite: {
+  base: "./",
+},
   tanstackStart: {
     prerender: {
       enabled: true,
